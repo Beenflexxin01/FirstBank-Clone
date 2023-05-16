5970fc9fa978089e48c1b00fffcc79456ec2d7e4
Adenola Omotayo <102471748+Beenflexxin01@users.noreply.github.com>
2023-05-16 16:20:05 +0100

    Initial commit

--- Js/index.js
+++ Js/index.js
@@ -1,7 +1,26 @@
-'use strict'
+'use strict';
 const btNav = document.querySelector('.btn-mobile-nav');
-const nav = document.querySelector('.header');
+const navHeader = document.querySelector('.header');
+const mainNav = document.querySelector('.main-nav')
+const mainHeader = document.querySelector('.main-header')
 
 btNav.addEventListener('click', function () {
-  nav.classList.toggle('nav-open');
+  navHeader.classList.toggle('nav-open');
 });
+
+// const navHeight = mainNav.getBoundingClientRect().height;
+// const stickyNav = function (entries) {
+//   const [entry] = entries;
+//   if (!entry.isIntersecting) {
+//     mainNav.classList.add('sticky');
+//   } else {
+//     mainNav.classList.remove('sticky')
+//   }
+// };
+
+// const headerObserver = new IntersectionObserver(stickyNav, {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: `${navHeight}px`,
+// });
+// headerObserver.observe(mainHeader);
