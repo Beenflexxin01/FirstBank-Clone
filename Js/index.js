5165d8aedb2b06418aaa5cc0351f0e66f37a9c05
Adenola Omotayo <102471748+Beenflexxin01@users.noreply.github.com>
2023-08-02 20:27:19 +0100

    Saved

--- Js/index.js
+++ Js/index.js
@@ -1,8 +1,8 @@
 'use strict';
 const btNav = document.querySelector('.btn-mobile-nav');
 const navHeader = document.querySelector('.header');
-const mainNav = document.querySelector('.main-nav')
-const mainHeader = document.querySelector('.main-header')
+const mainNav = document.querySelector('.main-nav');
+const mainHeader = document.querySelector('.main-header');
 
 btNav.addEventListener('click', function () {
   navHeader.classList.toggle('nav-open');
